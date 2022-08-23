4be138f287f30d12a9f560313c02dccb3e954cd5
Emma <ing.valen0421@hotmail.com>
2022-08-22 23:00:49 -0400

    fix: handle the version of react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider, connect } from 'react-redux';
+import store from "./rematch/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Count = (props) => (
+  <div>
+    The count is {props.count}
+    <button onClick={props.increment}>increment</button>
+    <button onClick={props.incrementAsync}>incrementAsync</button>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const mapState = (state) => ({
+  count: state.count,
+});
+
+const mapDispatch = (dispatch) => ({
+  increment: () => dispatch.count.increment(1),
+  incrementAsync: () => dispatch.count.incrementAsync(1),
+});
+
+const CountContainer = connect(mapState, mapDispatch)(Count);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <CountContainer />
+  </Provider>,
+  document.getElementById('root')
+);
+
